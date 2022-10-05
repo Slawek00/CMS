e2da258f0d9e28900854ef7efa1270a5bf4d0c34
Sławomir Jałojda <slawekjalojda@gmail.com>
2022-10-05 22:31:16 +0200

    Dodanie podstawowego menu i wystylowanie je

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
+  <AppMenu/>
+  <div class="sidebar-left">
 
+  </div>
+  <div class="add-content">
+
+  </div>
 </template>
 
 <script>
-
+import AppMenu from "@/components/AppMenu.vue";
 
 export default {
   name: 'App',
+  components:{
+    AppMenu
+  },
+  methods:{
 
+  }
 }
+
 </script>
 
 <style>
+#app{
+  display: flex;
+  flex-wrap: wrap;
+}
+
 
 </style>
